353514a5c1be74cdfd17d727f372d0714aa6a2f5
Amber Moroe <ambermoorexc@gmail.com>
2020-12-02 15:00:14 -0600

    Styling employees into cards

--- client/src/components/EmployeeCards/EmployeeCards.jsx
+++ client/src/components/EmployeeCards/EmployeeCards.jsx
@@ -7,13 +7,13 @@ function EmployeeCards(props) {
   
   return (
     <div className="employee-card">
-    <Link className="card" to={`/employees/${props._id}`} style={{textDecoration: 'none', color: 'black'}}>
-        <div className='employee-card-mask'>
-        <img className="employee-card-image" src={props.imgURL} alt={`${props.firstName} ${props.lastName}`} />
+    <Link className="employee-card-contents" to={`/employees/${props._id}`} style={{textDecoration: 'none', color: 'black'}}>
+        <div className='employee-content-mask'>
+        <img className="employee-content-image" src={props.imgURL} alt={`${props.firstName} ${props.lastName}`} />
         </div>
-        <div className='employee-card-name'>{props.firstName} {props.lastName}</div>
-        <div className='employee-card-position'>{props.position}</div>
-        <div className='employee-card-points'>{props.points}</div>
+        <div className='employee-content-name'>{props.firstName} {props.lastName}</div>
+        <div className='employee-content-position'>{props.position}</div>
+        <div className='employee-content-points'>{props.points}</div>
     </Link>
   </div>
   );
