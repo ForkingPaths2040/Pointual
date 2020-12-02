a07342362ce57e92fef8c53de7b3cdc40045b1cd
Amber Moroe <ambermoorexc@gmail.com>
2020-12-02 02:41:27 -0600

    Login screen is connected to backend and working

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,50 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
+
 import Login from './screens/Login/Login'
 
+import { loginUser, removeToken, verifyUser } from './services/auth';
+
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser();
+      setCurrentUser(userData);
+      if (!userData) {
+        history.push('/')
+      }
+    }
+    handleVerify();
+  }, [])
+
+  const handleLogin = async (loginData) => {
+    const userData = await loginUser(loginData);
+    setCurrentUser(userData);
+    history.push('/employees');
+  }
+
+  const handleLogout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem('authToken');
+    removeToken();
+    history.push('/');
+  }
+
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Switch>
+      <div className="App">
+        <Route path='/login'>
+          <Login
+            currentUser={currentUser}
+            handleLogin={handleLogin}
+          />
+        </Route>
+      </div>
+      </Switch>
   );
 }
 
--- client/src/screens/Login/Login.jsx
+++ client/src/screens/Login/Login.jsx
@@ -3,13 +3,14 @@ import Header from '../../components/Header/Header';
 import hero from '../../assets/images/candidates.png';
 import './Login.css'
 import LoginForm from '../../components/LoginForm/LoginForm';
-
 function Login(props) {
   return (
     <div>
       <Header />
       <div className='login-hero-container'>
-        <LoginForm />
+        <LoginForm
+          handleLogin={props.handleLogin}
+          currentUser={props.currentUser}/>
         <img className='hero-image' src={hero} alt="Reviewing candidates illustration" />
           <div className='hero-p'>
             <p className='medium-font' id='time-attendance'>Time and attendance</p>
