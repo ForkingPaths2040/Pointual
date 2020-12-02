f65e1975b26738556adb456ccda2e1c002d957d4
Amber Moroe <ambermoorexc@gmail.com>
2020-12-01 23:59:53 -0600

    Login screen designed and more file structure

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+function Header(props) {
+  return (
+    <div className='header-container'>
+      <h1 className='extra-bold' id='logo'>Pointual.</h1>
+    </div>
+  );
+}
+
+export default Header;
