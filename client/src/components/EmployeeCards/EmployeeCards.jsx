7a0a2c7595d5e8c5ddf6720c9757239f75aa8af5
Amber Moroe <ambermoorexc@gmail.com>
2020-12-07 09:20:43 -0600

    Points

--- client/src/components/EmployeeCards/EmployeeCards.jsx
+++ client/src/components/EmployeeCards/EmployeeCards.jsx
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom'
 import './EmployeeCards.css'
 
 
 function EmployeeCards(props) {
-  // const [points, setPoints] = useState(0);
-  
-  // useEffect(() => {
-  //   const handleSum = () => {
-  //     const totalInfractions = props.employee?.infractions?.map((infraction) => {
-  //       return infraction.points
-  //     })
-  //     const sum = totalInfractions?.reduce((sum, value) => sum + value)
-  //     setPoints(sum)
-  //   }
-  //   handleSum();
-  // }, [props.employee.infractions])
-  
+  const [points, setPoints] = useState(0);
+
+  useEffect(() => {
+    const handleSum = () => {
+      const totalInfractions = props.employee?.infractions?.map((infraction) => {
+        return infraction.points
+      })
+      if (totalInfractions.length) {
+        const sum = totalInfractions?.reduce((sum, value) => sum + value)
+        setPoints(sum)
+      }
+    }
+    handleSum();
+  }, [props.employee.infractions])
+
   return (
     <div className="employee-card">
-    <Link className="employee-card-contents" to={`/employees/${props._id}`} style={{textDecoration: 'none', color: 'black'}}>
+      <Link className="employee-card-contents" to={`/employees/${props._id}`} style={{ textDecoration: 'none', color: 'black' }}>
         <div className='card-content-mask'>
-        <img className="card-content-image" src={props.imgURL} alt={`${props.firstName} ${props.lastName}`} />
+          <img className="card-content-image" src={props.imgURL} alt={`${props.firstName} ${props.lastName}`} />
         </div>
         <div className='card-content-name'>{props.firstName} {props.lastName}</div>
         <div className='card-content-position'>{props.position}</div>
-        <div className='card-content-points'>{props.points} points</div>
-    </Link>
-  </div>
+        <div className='card-content-points'>{points} points</div>
+      </Link>
+    </div>
   );
 }
 
